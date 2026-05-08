6720b9bccd8ff699452688725f2e47c614cc6a0e
nabeel <nabeelsyed115@gmail.com>
2026-05-08 21:02:35 +0530

    open CORS to fix Vercel preflight errors

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,7 @@ if FRONTEND_URL:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
